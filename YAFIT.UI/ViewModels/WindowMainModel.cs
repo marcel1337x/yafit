f03c81ee6a0b53583f027756a4c7d268713bdcdc
marcel1337x <marcel.popp@gmail.com>
2025-01-21 13:09:14 +0100

    Login-Mock gebaut

--- YAFIT.UI/ViewModels/WindowMainModel.cs
+++ YAFIT.UI/ViewModels/WindowMainModel.cs
@@ -1,6 +1,12 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Security;
+using System.Security.Authentication;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using YAFIT.Databases.Classes;
 using YAFIT.UI.Resources;
+using YAFIT.UI.Views;
 
 namespace YAFIT.UI.ViewModels
 {
@@ -9,6 +15,8 @@ namespace YAFIT.UI.ViewModels
 
         public ICommand OnFeedbackCodeEnter { get; private set; }
         public ICommand OnAccountLogin { get; private set; }
+        private string _userName = "";
+        public string LoginUname { get { return _userName; }  set { SetProperty("LoginUname", ref _userName, value); } }
 
         public WindowMainModel(Window window) : base(window) {
             WindowCaption = "Yet Another Feeback Information Tool - YAFIT";
@@ -22,7 +30,18 @@ namespace YAFIT.UI.ViewModels
         }
         private void DoAccountLogin()
         {
-            MessageBox.Show("Login");
+            WindowMain? windowMain = _view as WindowMain;
+            Authentication auth = new Authentication(LoginUname, windowMain.PWBox.Password);
+            if (auth.doLogin())
+            {
+                MessageBox.Show("Login");
+            }
+            else
+            {
+                MessageBox.Show("Login Fehlerhaft");
+            }
+            Debug.WriteLine(LoginUname);
+            Debug.WriteLine(windowMain.PWBox.Password);
         }
     }
 }
