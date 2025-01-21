5a5c2ed54392636fb5b58bf2abe6d440db253795
jonashormess <jonas.hormess@web.de>
2025-01-21 10:49:09 +0100

    GUI Core Parts hinzugeügt Hauptfenster erster prototyp

---
+++ YAFIT.UI/Resources/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows.Input;
+
+namespace YAFIT.UI.Resources
+{
+    public class RelayCommand(Action action2execute, Func<bool>? canExecute = null) : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool IsExecutable
+        {
+            get { return _isExecutable; }
+            set
+            {
+                if (_isExecutable != value)
+                {
+                    _isExecutable = value;
+                    RaisCanExecuteChanged();
+                }
+            }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if (_canExecuteAction != null)
+            {
+                IsExecutable = _canExecuteAction();
+            }
+            return IsExecutable;
+
+        }
+
+        public void Execute(object? parameter)
+        {
+            _action2execute();
+        }
+
+        public void RaisCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+
+        private bool _isExecutable = true;
+        private readonly Action _action2execute = action2execute;
+        private readonly Func<bool>? _canExecuteAction = canExecute;
+    }
+}
---
+++ YAFIT.UI/ViewModels/WindowMainModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using YAFIT.UI.Resources;
+
+namespace YAFIT.UI.ViewModels
+{
+    public class WindowMainModel : BaseViewModel
+    {
+
+        public ICommand OnFeedbackCodeEnter { get; private set; }
+        public ICommand OnAccountLogin { get; private set; }
+
+        public WindowMainModel(Window window) : base(window) {
+            WindowCaption = "Yet Another Feeback Information Tool - YAFIT";
+            OnFeedbackCodeEnter = new RelayCommand(DoFeedbackCodeEnter);
+            OnAccountLogin = new RelayCommand(DoAccountLogin);
+        }
+
+        private void DoFeedbackCodeEnter()
+        {
+            MessageBox.Show("Feedback");
+        }
+        private void DoAccountLogin()
+        {
+            MessageBox.Show("Login");
+        }
+    }
+}
