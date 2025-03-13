033d3503b1a91e843b22a40d5a0e7a3ff04c7c70
jonashormess <jonas.hormess@web.de>
2025-03-13 13:48:51 +0100

    - Anpassung am Adminfenster - bei der Passwort eingabe einen Fehler behoben wenn der Wert kein BASE64 ist - Config Option hinzugefügt für die Testeinträge - Klasse angepasst für die generierung der Testdaten

---
+++ YAFIT.Databases/Extensions/UserEntityExtension.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using YAFIT.Databases.Entities;
+
+namespace YAFIT.Databases.Extensions
+{
+    public static class UserEntityExtension
+    {
+        public static bool IsPasswordValid(this UserEntity userEntity,string? password)
+        {
+            if (string.IsNullOrEmpty(password) == true)
+            {
+                return false;
+            }
+            if(PASSWORD_BASE64_PATTERN.IsMatch(userEntity.Password) == false)
+            {
+                return false;
+            }
+            byte[] userPasswordBase64 = Convert.FromBase64String(userEntity.Password);
+            byte[] salt = new byte[16];
+            try
+            {
+                Array.Copy(userPasswordBase64, 0, salt, 0, 16);
+            } catch(Exception e)
+            {
+
+                return false;
+            }
+            var pbkdf2 = new Rfc2898DeriveBytes(password ?? "", salt, 100000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            for (int i = 0; i < 20; i++)
+            {
+                if (userPasswordBase64[i + 16] != hash[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private static readonly Regex PASSWORD_BASE64_PATTERN = new Regex(@"^[a-zA-Z0-9\+/]*={0,2}$");
+    }
+}
